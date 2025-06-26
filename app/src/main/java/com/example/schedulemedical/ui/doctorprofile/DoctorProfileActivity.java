e4d39a15d70d49141745231c11c432cf6c9e1c90
lenvalen14 <nlhieunhi1402@gmail.com>
2025-06-26 07:21:53 +0700

    Fix UI home

---
+++ app/src/main/java/com/example/schedulemedical/ui/doctorprofile/DoctorProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.schedulemedical.ui.doctorprofile;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.schedulemedical.R;
+
+public class DoctorProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_profile);
+        // TODO: Add logic to populate doctor info
+    }
+} 
