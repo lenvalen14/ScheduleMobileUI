f83dadae9ddf70b35c4570d4cad3fbfd6fa1283a
devtrantrongtri <devtrantrongtri@gmail.com>
2025-07-08 19:57:43 +0700

    updateupdate

--- app/src/main/java/com/example/schedulemedical/ui/register/RegisterActivity.java
+++ app/src/main/java/com/example/schedulemedical/ui/register/RegisterActivity.java
@@ -1,6 +1,7 @@
 package com.example.schedulemedical.ui.register;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
